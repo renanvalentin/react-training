d267465fd2ca45fe14090b6556d31aba966ac52b
Renan Ferreira <renan.lifestyle@hotmail.com>
2017-01-20 22:39:12 -0200

    day two

--- demo/js/app.js
+++ demo/js/app.js
@@ -1,111 +1,118 @@
-function Done()
-{
-    clear();
+function done() {
+    const name = document.getElementById('name');
+    const salary = document.getElementById('salary');
+    const month = document.getElementById('month');
+    const vacation = document.getElementById("vacation");
 
-    /*if ((document.getElementById("name").value == "")
-     || (document.getElementById("salary").value == "")
-     || (document.getElementById("month").value == ""))*/
-    if (document.getElementById("name").value == "")
-    {
-        var name = 1
-        alertError(name);
-    }
-    else if(document.getElementById("salary").value == "")
-    {
-        var salary = 2
-        alertError(salary);
+    const validations = [{
+        field: name,
+        msg: 'Invalid name!'
+    }, {
+        field: salary,
+        msg: 'Invalid salary!'
+    }, {
+        field: month,
+        msg: 'Invalid month!',
+        customValidation: function(value) {
+            return isMonthValid(value);
+        }
+    }];
 
+    if (!validateFields(validations)) {
+        return false;
     }
-    else if((document.getElementById("month").value == "")
-        ||(document.getElementById("month").value < 1)
-        ||(document.getElementById("month").value > 12))
-    {
-        var month = 3
-        alertError(month);
-    }
-    else
-    {
-        var salary = document.getElementById("salary").value
-        var month = document.getElementById("month").value
-        var vacation = 'no';
 
-        if(document.getElementById("vacation").checked == 1)
-        {
-            vacation = 'yes';
+    const bonus = calcBonus(salary.value, month.value, vacation.checked);
+    const total = calcTotal(salary.value, month.value, bonus);
+
+    document.getElementById("bonus").value = bonus;
+    document.getElementById("total").value = total;
+
+    alertOk();
+}
+
+function validateFields(fields) {
+    return fields.every(function(item) {
+        if (isRequired(item) || !validateCustomValidation(item)) {
+            validate(item.field, item.msg);
+            return false;
+        } else {
+            removeInvalidClass(item.field);
         }
 
-        var bonus = parseFloat(calcBonus(salary,month,vacation).toFixed(2));
-
-        var total = parseFloat(calcTotal(salary,month,bonus).toFixed(2));
-
-        document.getElementById("bonus").value = bonus;
-        document.getElementById("total").value = total;
-
-        alertOk();
-
-    }
-
+        return true;
+    });
 }
 
-function alertError(msg)
-{
-    if(msg == 1)
-    {
-        alert('Invalid name!');
-        /*document.getElementById("name").style.backgroundColor = "red";*/
-        document.getElementById("name").className = "alert";
-        document.getElementById("name").focus();
+function isRequired(item) {
+    if (item.required != undefined) {
+        if (item.required) {
+            const wasFilled = isEmpty(item.field.value);
+            return wasFilled;
+        }
+
         return false;
     }
-    else if(msg == 2)
-    {
-        alert('Invalid Salary!');
-        /*document.getElementById("name").style.backgroundColor = "red";*/
-        document.getElementById("salary").className = "alert";
-        document.getElementById("salary").focus();
-        return false;
-    }
-    else if(msg == 3)
-    {
-        alert('Invalid Month!');
-        /*document.getElementById("name").style.backgroundColor = "red";*/
-        document.getElementById("month").className = "alert";
-        document.getElementById("month").focus();
-        return false;
-    }
+
+    return isEmpty(item.field.value);
 }
 
-function alertOk()
-{
-    alert('Hello' + ' ' + document.getElementById("name").value + ' Finished!');
-    /*document.getElementById("name").style.backgroundcolor = "transparent";*/
+function validateCustomValidation(item) {
+    if (item.customValidation) {
+        const isValid = item.customValidation(item.field.value);
+        return isValid;
+    }
+
     return true;
 }
 
-function clear()
-{
-    document.getElementById("name").className =     document.getElementById("name").className.replace( /(?:^|\s)alert(?!\S)/g , '' );
-    document.getElementById("salary").className =     document.getElementById("salary").className.replace( /(?:^|\s)alert(?!\S)/g , '' );
-    document.getElementById("month").className =     document.getElementById("month").className.replace( /(?:^|\s)alert(?!\S)/g , '' );
+function validate(field, msg) {
+    addInvalidClass(field);
+    focusInvalidField(field);
 }
 
-function calcBonus(salary,month,vacation)
-{
+function isEmpty(value) {
+    return value == '';
+}
+
+function isMonthValid(value) {
+    return value >= 1 && value <= 12;
+}
+
+function addInvalidClass(field) {
+    field.classList.add('alert');
+}
+
+function removeInvalidClass(field) {
+    field.classList.remove('alert');
+}
+
+function toggleValidateFieldStyle(field) {
+    field.classList.toggle('alert');
+}
+
+function focusInvalidField(field) {
+    field.focus();
+}
+
+function alertOk() {
+    alert('Hello' + ' ' + document.getElementById("name").value + ' Finished!');
+    return true;
+}
+
+function calcBonus(salary, month, vacation) {
     var bonus = 0;
+    const bonusPercentage = 0.275;
 
-    if(vacation == 'yes')
-    {
-        bonus = ((salary * (month - 1) ) * 0.275);
+    if (vacation) {
+        bonus = ((salary * (month - 1)) * bonusPercentage);
+    } else {
+        bonus = ((salary * month) * bonusPercentage);
     }
 
-    if(vacation == 'no')
-    {
-        bonus = ((salary * month) * 0.275);
-    }
-    return bonus;
+    return parseFloat(bonus).toFixed(2);
 }
 
-function calcTotal(salary,month,bonus)
-{
-    return ((salary * month) + bonus);
+function calcTotal(salary, month, bonus) {
+    return parseFloat(((salary * month) + bonus)).toFixed(2);
 }
